8192fa7c3f65df7f85717c5860424d5dc17a3316
SabaZumekan <new.camputer3@gmail.com>
2022-06-18 14:20:37 +0900

    App

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import Flask, request
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -33,3 +34,14 @@ def exercise_html():
 @app.route('/exercise')
 def exercise():
     return f'exercise:{request.args.get("my_name")}'
+
+@app.route('/try_rest', methods=['POST'])
+def try_rest():
+    # リクエストデータをJSONとして受け取る
+    request_json = request.get_json()
+    print(request_json)
+    print(type(request_json))
+    name = request_json['name']
+    print(name)
+    response_json = {"response_json": request_json}
+    return jsonify(response_json)
