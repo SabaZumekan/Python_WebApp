323036c27753840e79c8ea7fcd29703fd37fadf4
SabaZumekan <new.camputer3@gmail.com>
2022-06-18 14:42:57 +0900

    App

--- app.py
+++ app.py
@@ -45,3 +45,7 @@ def try_rest():
     print(name)
     response_json = {"response_json": request_json}
     return jsonify(response_json)
+
+@app.route('practice_rest' methods=['POST'])
+def practice_rest():
+    req_json = request.get_json() 
